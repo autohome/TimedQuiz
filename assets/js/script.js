647236d92657ccb59b14e73b7c070343e971371d
JoshuaD <joshdadbin@Josh.local>
2023-03-13 20:14:42 -0700

    cleaned HTML, event listener for start button, and first function after complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,8 @@ const quizQuestions = [
         answers: {
             a: "1939",
             b: "1949",
-            c: "1959"
+            c: "1959",
+            d: "1969"
         },
         correctAnswer: "a"
     },
@@ -14,7 +15,8 @@ const quizQuestions = [
         answers: {
             a: "Michael Keaton",
             b: "George Clooney",
-            c: "Val Kilmer"
+            c: "Val Kilmer",
+            d: "Danny DeVito"
         },
         correctAnswer: "c"
     },
@@ -23,7 +25,8 @@ const quizQuestions = [
         answers: {
             a: "The Joker",
             b: "The Riddler",
-            c: "The Penguin"
+            c: "The Penguin",
+            d: "Superman"
         },
         correctAnswer: "a"
     }
@@ -31,17 +34,60 @@ const quizQuestions = [
   
 // ELement selectors
 var startPage = document.querySelector('#startpage');
-var quizPage = document.querySelector ('#quiz');
-var endPage = document.querySelector ("end");
-var timerElement = document.querySelector ('#secondsleft');
+var quizContainer = document.querySelector ('#quizContainer');
+var questionContainer = document.querySelector ("questionContainer")
+var answerContainer = document.querySelector ("answerContainer")
+var endPage = document.querySelector ("#end");
+var timerElement = document.querySelector ("#secondsLeft");
+var startButton = document.querySelector("#startQuiz");
+var highScoresButton = document.querySelector("#highscores")
 
-var scores =[];
+
+var Score
+var storedScores =[];
 var timer;
 var timerCount;
+var currentQuestionIndex = 0;
 
+// function to clear page and start quiz
 function startGame () {
-    startPage.getElementsByClassName.display = "none";
+    startPage.setAttribute ("style", "display:none;");
+    highScoresButton.setAttribute("style", "visibility:hidden;");
+    // SET TIMER
+    timerCount = 30;
+    buildQuiz()
+    startTimer()
+}
+
+
+
+function startTimer () {
+    // run countdown
+    timer = setInterval(function() {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount === 0) {
+            return;
+        }
+    }, 1000);
+}
+
+
+function buildQuiz () {
+    // display quiz container
+    quizContainer.setAttribute("style", "display:contents;");
+
+    // get question from array and send to HTML
+    var currentQuestion = quizQuestions[currentQuestionIndex];
+    questionContainer.innerHTML = currentQuestion.question;
 
 }
+
+
+
+// Listen for start game and run
+startButton.addEventListener("click", startGame)
+
+
 
 
